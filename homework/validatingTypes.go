15bb7e08f6d433b5da4c52cb867f489eb701c6d0
bloodmain <krytoipohtalon@mail.ru>
2024-04-11 20:52:33 +0300

    Added validator

---
+++ homework/validatingTypes.go
@@ -0,0 +1,91 @@
+package homework
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type SupportedTypes interface {
+	int | string
+}
+
+type Validating[T SupportedTypes] interface {
+	Len(int) error
+	Min(int) error
+	Max(int) error
+	In(map[T]struct{}) error
+	Parse(string) (T, error)
+}
+
+type IntValidating int
+
+func (IntValidating) Len(int) error {
+	return fmt.Errorf("%w: operation \"len\" on type int", ErrUnsupportedOperationForType)
+}
+
+func (i IntValidating) Min(bound int) error {
+	if int(i) < bound {
+		return fmt.Errorf("%w: %d < %d", ErrMinValidationFailed, i, bound)
+	}
+	return nil
+}
+
+func (i IntValidating) Max(bound int) error {
+	if int(i) > bound {
+		return fmt.Errorf("%w: %d > %d", ErrMaxValidationFailed, i, bound)
+	}
+	return nil
+}
+
+func (i IntValidating) In(v map[int]struct{}) error {
+	if _, has := v[int(i)]; !has {
+		return fmt.Errorf("%w: %d is not in %s", ErrInValidationFailed, i, printMap(v))
+	}
+	return nil
+}
+
+func (i IntValidating) Parse(s string) (int, error) {
+	return strconv.Atoi(s)
+}
+
+type StringValidating string
+
+func (s StringValidating) Len(bound int) error {
+	if len(s) != bound {
+		return fmt.Errorf("%w: len(%s) == %d != %d", ErrLenValidationFailed, s, len(s), bound)
+	}
+	return nil
+}
+
+func (s StringValidating) Min(bound int) error {
+	if len(s) < bound {
+		return fmt.Errorf("%w: len(%s) == %d < %d", ErrMinValidationFailed, s, len(s), bound)
+	}
+	return nil
+}
+
+func (s StringValidating) Max(bound int) error {
+	if len(s) > bound {
+		return fmt.Errorf("%w: len(%s) == %d > %d", ErrMaxValidationFailed, s, len(s), bound)
+	}
+	return nil
+}
+
+func (s StringValidating) In(v map[string]struct{}) error {
+	if _, has := v[string(s)]; !has {
+		return fmt.Errorf("%w: \"%s\" is not in %s", ErrInValidationFailed, s, printMap(v))
+	}
+	return nil
+}
+
+func (s StringValidating) Parse(x string) (string, error) {
+	return x, nil
+}
+
+func printMap[T SupportedTypes](mp map[T]struct{}) string {
+	keys := make([]T, 0, len(mp))
+	for k := range mp {
+		keys = append(keys, k)
+	}
+	return fmt.Sprintf("%v", keys)
+}
---
+++ homework/validator.go
@@ -0,0 +1,158 @@
+package homework
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const (
+	validateTag  = "validate"
+	inOperation  = "in"
+	lenOperation = "len"
+	minOperation = "min"
+	maxOperation = "max"
+)
+
+var (
+	ErrNotStruct                   = errors.New("wrong argument given, should be a struct")
+	ErrInvalidValidatorSyntax      = errors.New("invalid validator syntax")
+	ErrValidateForUnexportedFields = errors.New("validation for unexported field is not allowed")
+	ErrLenValidationFailed         = errors.New("len validation failed")
+	ErrInValidationFailed          = errors.New("in validation failed")
+	ErrMaxValidationFailed         = errors.New("max validation failed")
+	ErrMinValidationFailed         = errors.New("min validation failed")
+	ErrUnsupportedType             = errors.New("unsupported type to validate")
+	ErrUnsupportedOperation        = errors.New("unsupported validation operation")
+	ErrUnsupportedOperationForType = errors.New("this operation is not supported for this type")
+)
+
+var SupportedOperations = []string{inOperation, lenOperation, minOperation, maxOperation}
+
+type ValidationError struct {
+	field string
+	err   error
+}
+
+func NewValidationError(err error, field string) error {
+	return &ValidationError{
+		field: field,
+		err:   err,
+	}
+}
+
+func (e *ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s", e.field, e.err)
+}
+
+func (e *ValidationError) Unwrap() error {
+	return e.err
+}
+
+func parseQuery(query string) (opName string, args []string, err error) {
+	ss := strings.Split(query, ":")
+	if len(ss) != 2 {
+		return "", nil, fmt.Errorf("%w: too many colons (%d)", ErrInvalidValidatorSyntax, len(ss)-1)
+	}
+	if !slices.Contains(SupportedOperations, ss[0]) {
+		return "", nil, fmt.Errorf("%w: unsupported operation (%s)", ErrUnsupportedOperation, ss[0])
+	}
+	if len(ss[1]) == 0 {
+		return "", nil, fmt.Errorf("%w: zero arguments for operation is provided", ErrInvalidValidatorSyntax)
+	}
+	args = strings.Split(ss[1], ",")
+	if ss[0] != inOperation && len(args) != 1 {
+		return "", nil, fmt.Errorf("%w: too many arguments (%d) for operation \"%s\"", ErrInvalidValidatorSyntax, len(args), ss[0])
+	}
+
+	return ss[0], args, nil
+}
+
+func executeValidating[T SupportedTypes](v Validating[T], name string, args []string) error {
+	if name == inOperation {
+		mp := map[T]struct{}{}
+		for _, s := range args {
+			x, e := v.Parse(s)
+			if e != nil {
+				return fmt.Errorf("%w: can't parse argument \"%s\" %w", ErrInvalidValidatorSyntax, s, e)
+			}
+			mp[x] = struct{}{}
+		}
+		return v.In(mp)
+	}
+
+	i, e := strconv.Atoi(args[0])
+	if e != nil {
+		return fmt.Errorf("%w: can't parse int argument \"%s\" %w", ErrInvalidValidatorSyntax, args[0], e)
+	}
+	switch name {
+	case lenOperation:
+		if i < 0 {
+			return fmt.Errorf("%w: negative value for len operation (%d)", ErrInvalidValidatorSyntax, i)
+		}
+		return v.Len(i)
+	case minOperation:
+		return v.Min(i)
+	case maxOperation:
+		return v.Max(i)
+	default:
+		return fmt.Errorf("%w: (%s)", ErrUnsupportedOperation, name)
+	}
+}
+
+func validateKind(v reflect.Value, opName string, args []string) error {
+	switch v.Kind() {
+	case reflect.Int:
+		return executeValidating[int](IntValidating(v.Int()), opName, args)
+	case reflect.String:
+		return executeValidating[string](StringValidating(v.String()), opName, args)
+	case reflect.Slice:
+		for i := range v.Len() {
+			e := validateKind(v.Index(i), opName, args)
+			if e != nil {
+				return e
+			}
+		}
+		return nil
+	default:
+		return fmt.Errorf("%w: (%s)", ErrUnsupportedType, v.Kind().String())
+	}
+}
+
+func validateQuery(v reflect.Value, query string) error {
+	name, args, e := parseQuery(query)
+	if e != nil {
+		return e
+	}
+	return validateKind(v, name, args)
+}
+
+func Validate(s any) error {
+	if reflect.ValueOf(s).Kind() != reflect.Struct {
+		return ErrNotStruct
+	}
+	st := reflect.TypeOf(s)
+	sv := reflect.ValueOf(s)
+
+	var es []error
+	for i := range st.NumField() {
+		field := st.Field(i)
+		if query, has := field.Tag.Lookup(validateTag); has {
+			if !field.IsExported() {
+				es = append(es, NewValidationError(ErrValidateForUnexportedFields, field.Name))
+			}
+			e := validateQuery(sv.Field(i), query)
+			if e != nil {
+				es = append(es, NewValidationError(e, field.Name))
+			}
+		}
+	}
+
+	if len(es) == 0 {
+		return nil
+	}
+	return errors.Join(es...)
+}
